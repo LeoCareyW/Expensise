d824e09dfdbf0d632ce7c4aa4a181d9a67267468
Leo Carey-Williams <leocareywilliams@gmail.com>
2020-08-21 16:03:28 +0100

    accumulator added

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
 import ExpenseListFilters from './ExpenseListFilters';
+import ExpensesTotal from './ExpensesTotal';
 
 const ExpenseDashboardPage = () => (
   <div>
     <ExpenseListFilters />
     <ExpenseList />
+    <ExpensesTotal />
   </div>
 );
 
